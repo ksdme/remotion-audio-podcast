0decdcbc5f84be95a019fcb87624cf2c6d66566d
Kilari Teja <kilariteja9@gmail.com>
2021-05-15 20:25:52 +0530

    Add pill color progress styling

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -1,18 +1,39 @@
 interface Props {
-  color?: string
-  height: number
-  width?: number
+	activeColor?: string
+	passiveColor?: string
+	height: number
+	width?: number
+	progress?: number
 }
 
-export default function Pill({ height, width = 60, color = 'white' }: Props) {
-  const pillStyle = {
-    height,
-    width,
-    background: color,
-    borderRadius: width / 2,
+export default function Pill({
+	height,
+	width = 60,
+	activeColor = 'white',
+	passiveColor = 'black',
+	progress = 1,
+}: Props) {
+  // Default background for 100% progress.
+	let background: string = activeColor
+
+	if (progress > 0 && progress < 1) {
+		background = `linear-gradient(
+      90deg,
+      ${activeColor} ${progress * 100}%,
+      ${passiveColor} ${(1 - progress) * 100}%
+    )`
+	}
+
+  if (progress === 0) {
+    background = passiveColor
   }
 
-  return (
-    <div style={pillStyle}></div>
-  )
+	const pillStyle = {
+		height,
+		width,
+		background,
+		borderRadius: width / 2,
+	}
+
+	return <div style={pillStyle}></div>
 }
