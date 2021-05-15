558af10952413aee72800387a249120be44ddcb4
Kilari Teja <kilariteja9@gmail.com>
2021-05-16 01:30:52 +0530

    Increase segment count to 40

--- prepare.py
+++ prepare.py
@@ -9,7 +9,7 @@ from scipy.io import wavfile
 # Configuration
 source_file_name = os.path.join(os.getcwd(), 'resources/audio.wav')
 target_file_name = f'{source_file_name}.json'
-segments_count = 32
+segments_count = 40
 
 # Read the file
 with open(source_file_name, 'rb') as source_file:
@@ -38,7 +38,7 @@ max_attainable = 100
 
 for index in range(len(segment_amplitudes)):
     factor = segment_amplitudes[index] / max_amplitude
-    segment_amplitudes[index] = factor* max_attainable
+    segment_amplitudes[index] = factor * max_attainable
 
 # Save the results
 with open(target_file_name, 'w') as target_file:
