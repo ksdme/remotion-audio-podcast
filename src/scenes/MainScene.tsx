bbe07251cb8d6b7f61a15fc31f00183ea0b293cb
Kilari Teja <kilariteja9@gmail.com>
2021-05-15 20:53:02 +0530

    Integrate partial calculations

--- src/scenes/MainScene.tsx
+++ src/scenes/MainScene.tsx
@@ -1,20 +1,26 @@
+import {useCurrentFrame, useVideoConfig} from 'remotion'
 import Pill from '../components/Pill'
 
-const samples = [
-	{ height: 200, progress: 1 },
-	{ height: 300, progress: 1 },
-	{ height: 200, progress: 1 },
-	{ height: 100, progress: 1 },
-	{ height: 400, progress: 1 },
-	{ height: 300, progress: 0.5 },
-	{ height: 400, progress: 0 },
-	{ height: 500, progress: 0 },
-	{ height: 200, progress: 0 },
-	{ height: 120, progress: 0 },
-	{ height: 80, progress: 0 },
+const amplitudes = [
+	95, 6, 89, 66, 21, 80, 57, 79, 60, 92, 17, 36, 44, 11, 3, 68, 28, 19, 99, 14,
+	81, 20, 40, 65, 79, 62, 25, 90, 80, 59,
 ]
 
-export default function MainScene() {
+interface Props {
+	samples: number[]
+	durationPerSample: number
+}
+
+export default function MainScene({ samples = amplitudes, durationPerSample = 2 }: Props) {
+	const frame = useCurrentFrame()
+	const video = useVideoConfig()
+	const minHeight = 20
+
+	// Calculations
+	const elapsed = video.fps * frame
+	const progress = elapsed / (samples.length * durationPerSample)
+
+	// Styling
 	const colors = {
 		backgroundColor: '#35D399',
 		activeColor: 'white',
@@ -34,22 +40,37 @@ export default function MainScene() {
 		gap: 12,
 	}
 
-	const pills = samples.map((sample, index) => {
+	const pillss = samples.map((sample, index) => {
+		const pillStarts = index * durationPerSample
+		const pillEnds = pillStarts + durationPerSample
+		let localProgress = 1
+
+		if (progress === 0) {
+			localProgress = 0
+		}
+
+		if (progress < pillStarts) {
+			localProgress = 0
+		}
+
+		if (progress > pillStarts && progress < pillEnds) {
+			localProgress = progress - pillStarts
+		}
+
 		return (
 			<Pill
 				key={index}
-				height={sample.height}
-				progress={sample.progress}
+				height={sample + minHeight}
+				progress={localProgress}
 				activeColor={colors.activeColor}
-				passiveColor={colors.passiveColor} />
+				passiveColor={colors.passiveColor}
+			/>
 		)
 	})
 
 	return (
 		<div style={canvasStyle}>
-			<div style={pillContainerStyle}>
-				{pills}
-			</div>
+			<div style={pillContainerStyle}>{pillss}</div>
 		</div>
 	)
 }
