25dc8049b150a4149aa735f370c1d5eeba61edbb
Kilari Teja <kilariteja9@gmail.com>
2021-05-15 20:07:35 +0530

    Add canvas color

--- src/scenes/MainScene.tsx
+++ src/scenes/MainScene.tsx
@@ -1,6 +1,16 @@
 export default function MainScene() {
+	const colors = {
+		backgroundColor: '#35D399',
+	}
+
+	const canvasStyle = {
+		height: '100%',
+		width: '100%',
+		background: colors.backgroundColor,
+	}
+
 	return (
-		<div>
+		<div style={canvasStyle}>
 			hello world
 		</div>
 	)
