813db4c36df73abe06c88b79acbc532125596cd7
Kilari Teja <kilariteja9@gmail.com>
2021-05-15 20:29:15 +0530

    Add samples

--- src/scenes/MainScene.tsx
+++ src/scenes/MainScene.tsx
@@ -1,8 +1,24 @@
 import Pill from '../components/Pill'
 
+const samples = [
+	{ height: 200, progress: 1 },
+	{ height: 300, progress: 1 },
+	{ height: 200, progress: 1 },
+	{ height: 100, progress: 1 },
+	{ height: 400, progress: 1 },
+	{ height: 300, progress: 0.5 },
+	{ height: 400, progress: 0 },
+	{ height: 500, progress: 0 },
+	{ height: 200, progress: 0 },
+	{ height: 120, progress: 0 },
+	{ height: 80, progress: 0 },
+]
+
 export default function MainScene() {
 	const colors = {
 		backgroundColor: '#35D399',
+		activeColor: 'white',
+		passiveColor: 'rgba(255, 255, 255, 0.6)',
 	}
 
 	const canvasStyle = {
@@ -18,16 +34,21 @@ export default function MainScene() {
 		gap: 12,
 	}
 
+	const pills = samples.map((sample, index) => {
+		return (
+			<Pill
+				key={index}
+				height={sample.height}
+				progress={sample.progress}
+				activeColor={colors.activeColor}
+				passiveColor={colors.passiveColor} />
+		)
+	})
+
 	return (
 		<div style={canvasStyle}>
 			<div style={pillContainerStyle}>
-				<Pill height={200} />
-				<Pill height={300} />
-				<Pill height={400} />
-				<Pill height={500} />
-				<Pill height={200} />
-				<Pill height={200} />
-				<Pill height={300} />
+				{pills}
 			</div>
 		</div>
 	)
